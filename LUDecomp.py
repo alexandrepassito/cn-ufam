bfa236fa58ed1f712ee9c2cc4c8d270ab8d147f1
alexandrepassito <72059492+alexandrepassito@users.noreply.github.com>
2022-11-10 09:00:33 -0400

    Create LUDecomp.py

---
+++ LUDecomp.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def LUdecomp(a):
+    n = len(a)
+    for k in range(0,n-1):
+        for i in range(k+1,n):
+           if a[i,k] != 0.0:
+               lam = a [i,k]/a[k,k]
+               a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+               a[i,k] = lam
+    return a
+
+def LUsolve(a,b):
+    n = len(a)
+    for k in range(1,n):
+        b[k] = b[k] - np.dot(a[k,0:k],b[0:k])
+    b[n-1] = b[n-1]/a[n-1,n-1]
+    for k in range(n-2,-1,-1):
+       b[k] = (b[k] - np.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
+    return b
+    
+ z = np.array([[3.0, -1.0, 4.0],
+             [-2.0, 0.0, 5.0],
+             [7.0, 2.0, -2.0]])
+
+k = np.array([[6.0, 3.0, 7.0]])
+
+z = LUdecomp(z)
+x = LUsolve(z,k[0])
+
+print('x = \n', x)
